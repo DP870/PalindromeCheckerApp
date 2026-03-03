47bb17c6be861a276dacfaa526fea740b87ae93f
pisharody <dpis3003@7-11.com>
2026-03-03 16:28:32 +0530

    [Dhruv] Application intialization and entry

---
+++ src/com/Palindrome/PalindromeChecker.java
@@ -0,0 +1,25 @@
+/* Main Class - UseCasePalindromeApp
+ * 
+ * Use Case 1: Application Entry and Welcome Message
+ * 
+ * Description:
+ * This class represents the entry point of the Palindrome 
+ * Checker Management System.
+ * 
+ * Entry Point of the Palindrome Checker Management.
+ * 
+ * @author Dhruv
+ * @version 1.0
+ * 
+ */
+
+package com.Palindrome;
+
+public class PalindromeChecker {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Palindrome App");
+		System.out.println("Version: 1.0");
+		System.out.println("System intializaed successfully");
+	}
+}
